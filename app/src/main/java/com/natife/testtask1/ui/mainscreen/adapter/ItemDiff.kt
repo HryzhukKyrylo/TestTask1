905e621270ebef9ce7982cd6a36daf4fd0e47808
HryzhukKyrylo <kaktyss121@gmail.com>
2021-06-23 10:51:38 +0300

    refactor ItemDiff

--- app/src/main/java/com/natife/testtask1/ui/mainscreen/adapter/ItemDiff.kt
+++ app/src/main/java/com/natife/testtask1/ui/mainscreen/adapter/ItemDiff.kt
@@ -6,10 +6,10 @@ import com.natife.testtask1.data.Item
 
 class ItemDiff : DiffUtil.ItemCallback<Item>() {
     override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
-        return true
+        return oldItem.id == newItem.id
     }
 
     override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
-        return true
+        return oldItem == newItem
     }
 }
